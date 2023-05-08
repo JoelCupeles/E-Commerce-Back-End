2b90709c4010e79080ad1c527cbf6f4703fb8b27
JoelCupeles <joel.cupeles1998@gmail.com>
2023-05-08 18:14:59 -0500

    Associations with routes API update

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, through: ProductTag }]
+      include: [{ model: Product, as: 'products' }]
     });
     res.json(tagData);
   } catch (err) {
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag }]
+      include: [{ model: Product, as: 'products' }]
     });
 
     if (!tagData) {
